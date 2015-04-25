eab80c58acc3ad7b5eaa7246d585ac7e5531d88c
chris-wituchowski <c.wituchowski@gmail.com>
2015-04-24 23:02:03 -0700

    next iteration of website with new layout using bootstrap and jquery

--- script.js
+++ script.js
@@ -1,49 +1,26 @@
 $(document).ready(function() {
-  var previousContent = "#aboutcontent";
-      $('#aboutbutton').hover(function() {
-          $(this).css("background-color", "#00cc00");
-          console.log(previousContent);
-        },
-        function() {
-          $(this).css("background-color", "#FFFFFF");
+  $('#resume-content').hide();
+  $('#contact-content').hide();
+
+  console.log("ready");
+  var previousContent = "about";
+  function showHide(navDescriptor) {
+    if (previousContent != (navDescriptor)) {
+      //$('#' + navDescriptor + '-content').removeClass("hidden");
+      $('#' + navDescriptor + '-content').show("fast");
+      //$('#' + previousContent + '-content').addClass("hidden");
+      $('#' + previousContent + '-content').hide("fast");
+      previousContent = navDescriptor;
+      console.log("test " + navDescriptor);
+    }
+  }
+      $('#about-button').click(function() {
+        showHide('about');
       });
-      $('#aboutbutton').click(function() {
-        $('#aboutcontent').fadeToggle("slow");
-        $(previousContent).fadeToggle("slow");
-        previousContent = '#aboutcontent';
+      $('#resume-button').click(function() {
+        showHide('resume');
       });
-      $('#infobutton').hover(function() {
-        $(this).css("background-color", "#00cc00");
-        console.log("resume hover");
-      },
-      function() {
-        $(this).css("background-color", "#FFFFFF");
-      });
-      $('#workbutton').hover(
-        function() {
-          $(this).css("background-color", "#00cc00");
-          console.log("about hover");
-        },
-        function() {
-          $(this).css("background-color", "#FFFFFF");
-      });
-      $('#playbutton').hover(function() {
-        $(this).css("background-color", "#00cc00");
-        console.log("resume hover");
-      },
-      function() {
-        $(this).css("background-color", "#FFFFFF");
-      });
-      $('#contactbutton').hover(function() {
-        $(this).css("background-color", "#00cc00");
-        console.log("resume hover");
-      },
-      function() {
-        $(this).css("background-color", "#FFFFFF");
-      });
-      $('#contactbutton').click(function() {
-        $('#contactcontent').fadeToggle("slow");
-        $(previousContent).fadeToggle("slow");
-        previousContent = '#contactcontent';
+      $('#contact-button').click(function() {
+        showHide('contact');
       });
 });
